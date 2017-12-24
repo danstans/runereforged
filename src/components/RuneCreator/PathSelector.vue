01fceb0b407e928cc55a30b18d0a9e00d02c2c5a
Daniel Stansberry <stansberry2295@gmail.com>
2017-12-23 19:45:11 -0800

    added static images for the path selector

--- src/components/RuneCreator/PathSelector.vue
+++ src/components/RuneCreator/PathSelector.vue
@@ -1,19 +1,139 @@
 <template>
   <div class="path-selector">
-    <div class="rune-category">
-      Precision
+    <div class="path-category">
+      <div class="path-top">
+        <div class="path-image">
+          <img src="../../assets/precision/precision.png" alt="asdf">
+        </div>
+      </div>
+      <div class="path-middle">
+        <span class="path-title">Precision</span>
+        <span class="path-subtitle">Become a Legend</span>
+      </div>
+      <div class="path-bottom">
+        <div class="keystones">
+          <div class="icon" id="icon-1">
+            <img src="../../assets/precision/keystones/PressTheAttack.png" alt="">            
+          </div>
+          <div class="icon" id="icon-2">
+            <img src="../../assets/precision/keystones/LethalTempo.png" alt="">            
+          </div>
+          <div class="icon" id="icon-3">
+            <img src="../../assets/precision/keystones/FleetFootwork.png" alt="">            
+          </div>
+        </div>
+        <div class="path-footer">
+          Improved attacks and sustained damage
+        </div>
+      </div>
     </div>
-    <div class="rune-category">
-      Domination
+    <div class="path-category">
+      <div class="path-top">
+        <div class="path-image">
+          <img src="../../assets/domination/domination.png" alt="asdf">
+        </div>
+      </div>
+      <div class="path-middle">
+        <span class="path-title">Domination</span>
+        <span class="path-subtitle">Hunt and Eliminate Prey</span>
+      </div>
+      <div class="path-bottom">
+        <div class="keystones">
+          <div class="icon" id="icon-1">
+            <img src="../../assets/domination/keystones/Electrocute.png" alt="">            
+          </div>
+          <div class="icon" id="icon-2">
+            <img src="../../assets/domination/keystones/Predator.png" alt="">            
+          </div>
+          <div class="icon" id="icon-3">
+            <img src="../../assets/domination/keystones/DarkHarvest.png" alt="">            
+          </div>
+        </div>
+        <div class="path-footer">
+          Burst damage and target access
+        </div>
+      </div>
     </div>
-    <div class="rune-category">
-      Sorcery
+    <div class="path-category">
+      <div class="path-top">
+        <div class="path-image">
+          <img src="../../assets/sorcery/sorcery.png" alt="asdf">
+        </div>
+      </div>
+      <div class="path-middle">
+        <span class="path-title">Sorcery</span>
+        <span class="path-subtitle">Unleash Destruction</span>
+      </div>
+      <div class="path-bottom">
+        <div class="keystones">
+          <div class="icon" id="icon-1">
+            <img src="../../assets/sorcery/keystones/SummonAerie.png" alt="">            
+          </div>
+          <div class="icon" id="icon-2">
+            <img src="../../assets/sorcery/keystones/ArcaneComet.png" alt="">            
+          </div>
+          <div class="icon" id="icon-3">
+            <img src="../../assets/sorcery/keystones/PhaseRush.png" alt="">            
+          </div>
+        </div>
+        <div class="path-footer">
+          Empowered abilities and resource manipulation
+        </div>
+      </div>
     </div>
-    <div class="rune-category">
-      Resolve
+    <div class="path-category">
+      <div class="path-top">
+        <div class="path-image">
+          <img src="../../assets/resolve/resolve.png" alt="asdf">
+        </div>
+      </div>
+      <div class="path-middle">
+        <span class="path-title">Resolve</span>
+        <span class="path-subtitle">Live Forever</span>
+      </div>
+      <div class="path-bottom">
+        <div class="keystones">
+          <div class="icon" id="icon-1">
+            <img src="../../assets/resolve/keystones/GraspOfTheUndying.png" alt="">            
+          </div>
+          <div class="icon" id="icon-2">
+            <img src="../../assets/resolve/keystones/Guardian.png" alt="">            
+          </div>
+          <div class="icon" id="icon-3">
+            <img src="../../assets/resolve/keystones/Aftershock.png" alt="">            
+          </div>
+        </div>
+        <div class="path-footer">
+          Durability and crowd control
+        </div>
+      </div>
     </div>
-    <div class="rune-category">
-      Inspiration
+    <div class="path-category">
+      <div class="path-top">
+        <div class="path-image">
+          <img src="../../assets/inspiration/inspiration.png" alt="asdf">
+        </div>
+      </div>
+      <div class="path-middle">
+        <span class="path-title">Inspiration</span>
+        <span class="path-subtitle">Outwit mere mortals</span>
+      </div>
+      <div class="path-bottom">
+        <div class="keystones">
+          <div class="icon" id="icon-1">
+            <img src="../../assets/inspiration/keystones/GlacialAugment.png" alt="">            
+          </div>
+          <div class="icon" id="icon-2">
+            <img src="../../assets/inspiration/keystones/UnsealedSpellbook.png" alt="">            
+          </div>
+          <div class="icon" id="icon-3">
+            <img src="../../assets/inspiration/keystones/Kleptomancy.png" alt="">            
+          </div>
+        </div>
+        <div class="path-footer">
+          Durability and crowd control
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -33,9 +153,84 @@
   flex-direction: row;
   justify-content: flex-start;
 
-  .rune-category {
+  .path-category {
     width: 20%;
-    border: .1px solid white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    .path-top {
+      height: 20%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+
+      .path-image {
+        cursor: pointer;
+        background: url('http://d181w3hxxigzvh.cloudfront.net/wp-content/themes/rune_forge/imgs/rune-paths/ring.png');
+        // background: url('http://d181w3hxxigzvh.cloudfront.net/wp-content/themes/rune_forge/imgs/rune-paths/vfx-p.png');
+        background-repeat: no-repeat;
+        background-position: center center;
+      }
+    }
+
+
+    .path-middle {
+      font-family: Helvetica, sans-serif , serif;
+      text-transform: uppercase;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      .path-title {
+        margin-bottom: 10px;
+        font-size: 1.4rem;
+      }
+      .path-subtitle {
+        font-size: .9rem;
+      }
+    }
+
+    .path-bottom {
+      height: 50%;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;
+
+      .keystones {
+        flex-grow: 1;
+        position: relative;
+
+        .icon {
+          position: absolute;
+          height: 75px;
+          width: 75px;
+
+          img {
+            max-height: 100%;
+            max-width: 100%;
+          }
+        }
+        #icon-1 {
+          left: 20px;
+          top: 22%;
+        }
+        #icon-2 {
+          right: 20px;
+          top: 22%;
+        }
+
+        #icon-3 {
+          bottom: 22%;  
+          left: 0;
+          right:0;
+          margin: 0 auto;
+        }
+      }
+
+      .path-footer {
+        height: 10%;
+      }
+    }
   }
 }
 </style>
