9327a64e6c82f210ed74944d285b23b7b344fa76
Daniel Stansberry <stansberry2295@gmail.com>
2017-12-23 21:39:37 -0800

    changed background and hover properties

--- src/components/RuneCreator/PathSelector.vue
+++ src/components/RuneCreator/PathSelector.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="path-selector">
     <div class="path-category">
+      <img src="https://lolstatic-a.akamaihd.net/frontpage/apps/prod/preseason-2018/en_US/a6708b7ae3dbc0b25463f9c8e259a513d2c4c7e6/assets/img/runeBuilder/assets/Precision/glow-p.png" alt="">      
       <div class="path-top">
         <div class="path-image">
           <img src="../../assets/precision/precision.png" alt="asdf">
@@ -12,7 +13,7 @@
       </div>
       <div class="path-bottom">
         <div class="keystones">
-          <div class="icon" id="icon-1">
+          <div class="icon" id="icon-1" @click="checkClick">
             <img src="../../assets/precision/keystones/PressTheAttack.png" alt="">            
           </div>
           <div class="icon" id="icon-2">
@@ -28,6 +29,7 @@
       </div>
     </div>
     <div class="path-category">
+      <img src="https://lolstatic-a.akamaihd.net/frontpage/apps/prod/preseason-2018/en_US/a6708b7ae3dbc0b25463f9c8e259a513d2c4c7e6/assets/img/runeBuilder/assets/Domination/glow-d.png" alt="">
       <div class="path-top">
         <div class="path-image">
           <img src="../../assets/domination/domination.png" alt="asdf">
@@ -55,6 +57,7 @@
       </div>
     </div>
     <div class="path-category">
+      <img src="https://lolstatic-a.akamaihd.net/frontpage/apps/prod/preseason-2018/en_US/a6708b7ae3dbc0b25463f9c8e259a513d2c4c7e6/assets/img/runeBuilder/assets/Sorcery/glow-s.png" alt="">
       <div class="path-top">
         <div class="path-image">
           <img src="../../assets/sorcery/sorcery.png" alt="asdf">
@@ -82,6 +85,7 @@
       </div>
     </div>
     <div class="path-category">
+      <img src="https://lolstatic-a.akamaihd.net/frontpage/apps/prod/preseason-2018/en_US/a6708b7ae3dbc0b25463f9c8e259a513d2c4c7e6/assets/img/runeBuilder/assets/Resolve/glow-r.png" alt="">
       <div class="path-top">
         <div class="path-image">
           <img src="../../assets/resolve/resolve.png" alt="asdf">
@@ -109,6 +113,7 @@
       </div>
     </div>
     <div class="path-category">
+      <img src="https://lolstatic-a.akamaihd.net/frontpage/apps/prod/preseason-2018/en_US/a6708b7ae3dbc0b25463f9c8e259a513d2c4c7e6/assets/img/runeBuilder/assets/Inspiration/glow-i.png" alt="">
       <div class="path-top">
         <div class="path-image">
           <img src="../../assets/inspiration/inspiration.png" alt="asdf">
@@ -140,7 +145,12 @@
 
 <script>
   export default {
-    name: 'PathSelector'
+    name: 'PathSelector',
+    methods: {
+      checkClick: function () {
+        console.log('you clicked')
+      }
+    }
   }
 </script>
 
@@ -158,6 +168,24 @@
     display: flex;
     flex-direction: column;
     justify-content: center;
+    background: url('https://lolstatic-a.akamaihd.net/frontpage/apps/prod/preseason-2018/en_US/a6708b7ae3dbc0b25463f9c8e259a513d2c4c7e6/assets/img/runeBuilder/assets/lines.png');
+    background-repeat: no-repeat;
+    background-position: top center;
+    position: relative;
+
+    >img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 100%;
+      filter: brightness(50%);
+      z-index: 1;
+      cursor: pointer;
+      &:hover {
+        filter: brightness(100%);
+      }
+    }
 
     .path-top {
       height: 20%;
@@ -166,11 +194,11 @@
       justify-content: center;
 
       .path-image {
-        cursor: pointer;
         background: url('http://d181w3hxxigzvh.cloudfront.net/wp-content/themes/rune_forge/imgs/rune-paths/ring.png');
-        // background: url('http://d181w3hxxigzvh.cloudfront.net/wp-content/themes/rune_forge/imgs/rune-paths/vfx-p.png');
         background-repeat: no-repeat;
         background-position: center center;
+        cursor: pointer;
+
       }
     }
 
@@ -181,6 +209,7 @@
       display: flex;
       flex-direction: column;
       justify-content: center;
+
       .path-title {
         margin-bottom: 10px;
         font-size: 1.4rem;
@@ -204,23 +233,26 @@
           position: absolute;
           height: 75px;
           width: 75px;
+          cursor: pointer;
+          // z-index: 1;
 
           img {
             max-height: 100%;
             max-width: 100%;
           }
         }
+
         #icon-1 {
-          left: 20px;
-          top: 22%;
+          left: 42px;
+          top: 15%;
         }
         #icon-2 {
-          right: 20px;
-          top: 22%;
+          right: 42px;
+          top: 15%;
         }
 
         #icon-3 {
-          bottom: 22%;  
+          bottom: 35%;  
           left: 0;
           right:0;
           margin: 0 auto;
